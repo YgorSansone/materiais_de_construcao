cdc163041c8e7c490340290098f730929acc91fd
Ygor Sansone <ygorsansone@gmail.com>
2020-10-07 16:01:14 -0300

    consultas

---
+++ consultas/consulta.php
@@ -0,0 +1,19 @@
+<?php 
+// if ($_GET['func'] == 'buscar') {
+        $obj = json_decode($_POST['val']);
+        echo "<pre>";
+        print_r($_POST['val']);
+        echo "</pre>";
+        echo $_POST['val'];
+        // $obj;
+        // $dados['id_video'] = $obj->id_video;
+        // $dados['id_user'] = $obj->id_user;
+
+        // $retorno = $__cons->ConsultaCursoVideo('%', $obj->id_user, $obj->id_video);
+
+        // $retorno[0]['url'] = $__html->_aws_bucket . $retorno[0]['caminho'];
+
+        // echo json_encode((object)$retorno);
+        //echo json_encode($dados['descricao']);
+// }
+?>
