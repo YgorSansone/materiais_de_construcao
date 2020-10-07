3aa7c9c3186461567e5d3eac09cd78dc2f5980e8
Ygor Sansone <ygorsansone@gmail.com>
2020-10-07 20:50:28 -0300

    buscar todos os materiais

--- config/conexaodb.php
+++ config/conexaodb.php
@@ -2,7 +2,7 @@
 try {
   $_con = new PDO("mysql:host=sql10.freesqldatabase.com;dbname=sql10369355;charset=utf8", "sql10369355", "fsr5U5FKKS", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
   $_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $SELECT = $_con->prepare("SELECT * from material");
+  $SELECT = $_con->prepare("SELECT * from material ORDER BY id ASC");
   // var_dump($SELECT);
   // print_r($SELECT);
   // $getUsers = $DBH->prepare("SELECT * FROM users ORDER BY id ASC");
