fd9650c0655b7498763c9182a78b06e8ea1a16f7
Ygor Sansone <ygorsansone@gmail.com>
2020-10-07 21:32:17 -0300

    refatorando

--- config/conexaodb.php
+++ config/conexaodb.php
@@ -2,15 +2,7 @@
 try {
   $_con = new PDO("mysql:host=sql10.freesqldatabase.com;dbname=sql10369355;charset=utf8", "sql10369355", "fsr5U5FKKS", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
   $_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $SELECT = $_con->prepare("SELECT * from material ORDER BY id ASC");
-  // var_dump($SELECT);
-  // print_r($SELECT);
-  // $getUsers = $DBH->prepare("SELECT * FROM users ORDER BY id ASC");
-  $SELECT->execute();
-  $users = $SELECT->fetchAll();
-  foreach ($users as $user) {
-    echo '<br>' . $user['nome'];
-  }
+
 
   // while ($linha = $SELECT->fetch(PDO::FETCH_ASSOC)) {
   //   echo "{$linha['nome']}</br>";
--- consultas/consulta.php
+++ consultas/consulta.php
@@ -1,4 +1,5 @@
 <?php 
+require_once("../config/conexaodb.php");
     function objectToArray($d) {
         if (is_object($d)) {
             $d = get_object_vars($d);
@@ -11,13 +12,31 @@
             return $d;
         }
     }
-    
+
         $obj = json_decode($_POST['val']);
+        $materiais = array();
+        $quantidade = array();
         if (is_object($obj)) {
                 $objj = objectToArray($obj);
         foreach ($objj as $key => $value) {
                 echo "Nome:{$key}, Quantidade:{$value} </br>";
+                array_push($materiais, $key);
+                array_push($quantidade, $value);
+                $SELECT = $_con->prepare("SELECT * FROM deposito WHERE id IN (SELECT codDeposito FROM estoque where codMaterial IN (SELECT id from material WHERE nome = '$key'))");
+                // SELECT * FROM table1 WHERE id IN (SELECT id FROM table2);
                 print_r($arr);
             }
+        }
+        // print_r($materiais);
+        // print_r($quantidade);
+
+        
+        // var_dump($SELECT);
+        // print_r($SELECT);
+        // $getUsers = $DBH->prepare("SELECT * FROM users ORDER BY id ASC");
+        $SELECT->execute();
+        $users = $SELECT->fetchAll();
+        foreach ($users as $user) {
+          echo '<br>' . $user['nome'];
         }
 ?>
