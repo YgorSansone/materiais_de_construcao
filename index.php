d6bfaf4a29b3a726e2072464a99f162ebc04bf18
Ygor Sansone <ygorsansone@gmail.com>
2020-10-10 21:58:59 -0300

    chamar o js de ordernar tabela

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ require_once("config/conexaodb.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/ordernartabela.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 
@@ -45,6 +46,7 @@ require_once("config/conexaodb.php");
 </div>
 
     <script src="js/opencsv.js"></script>
+    
 </body>
 
 </html>
